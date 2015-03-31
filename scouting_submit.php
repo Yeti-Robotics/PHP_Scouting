3ada0a0de0f8602bf6c21eb89b5fd635e2170f48
Trevor Daino <dainotrevor@gmail.com>
2015-03-30 21:26:39 -0400

    - Added file to submit data into SQL database

--- scouting_submit.php
+++ scouting_submit.php
@@ -1,3 +1,39 @@
 <?php
-print_r($_REQUEST);
+print_r ( $_REQUEST );
+include ("connect.php");
+
+$query = "INSERT INTO scout_data (team, match_number,
+		 robot_moved, totes_auto, cans_auto, coopertition,
+		 coopertition_totes, score) 
+		VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
+
+if($stmt = $db->prepare($query)){
+	$robot_moved = 0;
+	if(isset($_POST["robot_moved"])){
+		$robot_moved = 1;
+	} else{
+		$robot_moved = 0;
+	}
+	$coopertition_number = 0;
+	if(isset($_POST["coopertition_number"])){
+		$coopertition_number = 1;
+	} else{
+		$coopertition_number = 0;
+	}
+	$stmt->bind_param("iiiiiiii", $_POST["match_number"],
+		$_POST["team_number"],
+		$robot_moved,
+		$_POST["totes_auto"],
+		$_POST["cans_auto"],
+		$coopertition_number,
+		$_POST["coopertition_totes"],
+		$_POST["score"]);
+	
+	$stmt->execute();
+	$insert_id = $stmt->insert_id;
+	echo($insert_id);
+	
+}
+$db->close();
 ?>
+
