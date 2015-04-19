a94593664ef13b14da0815e55a8315c9f5a30b83
Sam Perlmutter <sam.perlmutter227@gmail.com>
2015-04-19 17:13:53 -0400

    - results.php, Fixed the query

--- results.php
+++ results.php
@@ -4,26 +4,20 @@
 	<div class="results_description">Click a row to view detailed data on that team.</div>
 <?php
 include ("connect.php");
-$rating_query = "SELECT AVG(rating) AS rating, team
+$query = "SELECT t1.team AS Team, t1.avg_height AS 'Avg. Stack Height', t2.avg_stacks AS 'Avg. Stacks per Match', t1.avg_height * t2.avg_stacks AS 'Avg. Totes per Match', rating AS 'Rating'
+FROM (SELECT team, AVG(totes) AS avg_height
+FROM stacks
+LEFT JOIN scout_data ON scout_data.scout_data_id=stacks.scout_data_id
+GROUP BY team) AS t1
+LEFT JOIN (SELECT team, COUNT(totes > 0) / COUNT(DISTINCT match_number) AS avg_stacks
+FROM stacks
+RIGHT JOIN scout_data ON scout_data.scout_data_id = stacks.scout_data_id
+GROUP BY team
+ORDER BY team DESC) AS t2 ON t1.team = t2.team
+LEFT JOIN (SELECT AVG(rating) AS rating, team
 					FROM scout_data
-					GROUP BY team";
-$result = $db->query($rating_query);
-$ratings = [];
-if ($result) {
-	while ( $row = $result->fetch_assoc () ) {
-		$ratings[$row["team"]] = $row["rating"];
-	}
-} else {
-	$db->close ();
-	die ( "<h2>Rating query failed</h2>" );
-}
-$query = "SELECT Team, TRUNCATE(SUM(avg_height)/SUM(num_stacks),2) AS \"Avg. Stack Height\", TRUNCATE(AVG(num_stacks),2) AS \"Avg. Stacks per Match\", TRUNCATE((AVG(num_stacks) * SUM(avg_height)/SUM(num_stacks)),2) AS \"Avg. Totes per Match\"
-                FROM (SELECT scout_data.*, COUNT(totes) AS num_stacks, IFNULL(AVG(totes),0) AS avg_height
-                        FROM stacks
-                        RIGHT JOIN scout_data ON scout_data.scout_data_id = stacks.scout_data_id
-                        GROUP BY team, match_number) AS t
-                GROUP BY Team
-                ORDER BY \"Avg. Totes per Match\" DESC";
+					GROUP BY team) AS t3 ON t1.team=t3.team
+ORDER BY 'Avg. Totes per Match' DESC";
 $result = $db->query ( $query );
 if ($result) {
 	echo "<table border='1'>";
@@ -32,14 +26,12 @@ if ($result) {
 	foreach ($fields as $field) {
 		echo "<th>".$field->name."</th>";
 	}
-	echo "<th>Rating</th>";	
 	echo "</tr>";
 	while ( $row = $result->fetch_assoc () ) {
 		echo "<tr class=\"results_row\" onclick=\"document.location='team.php?team=".$row["Team"]."'\">";
 		foreach ( $row as $key => $value ) {
 			echo "<td>" . $value . "</td>";
 		}
-		echo "<td>".$ratings[$row["Team"]]."</td>";
 		echo "</tr>";
 	}
 	echo "</table>";
