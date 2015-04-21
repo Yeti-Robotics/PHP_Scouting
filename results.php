9c234a347db6576a215f156f4cf5493e2d0ee415
Sam Perlmutter <sam.perlmutter227@gmail.com>
2015-04-21 19:51:18 -0400

    - results.php, Changed Avg. Stack Height to Highest Stack Made

--- results.php
+++ results.php
@@ -4,8 +4,8 @@
 	<div class="results_description">Click a row to view detailed data on that team.</div>
 <?php
 include ("connect.php");
-$query = "SELECT t1.team AS Team, ROUND(t1.avg_height,2) AS 'Avg. Stack Height', ROUND(t2.avg_stacks,2) AS 'Avg. Stacks per Match', ROUND(t1.avg_height * t2.avg_stacks,2) AS 'Avg. Totes per Match', ROUND(rating,2) AS 'Rating'
-FROM (SELECT team, AVG(totes) AS avg_height
+$query = "SELECT t1.team AS Team, ROUND(t1.avg_height,2) AS 'Highest Stack Made', ROUND(t2.avg_stacks,2) AS 'Avg. Stacks per Match', ROUND(t1.avg_height * t2.avg_stacks,2) AS 'Avg. Totes per Match', ROUND(rating,2) AS 'Rating'
+FROM (SELECT team, MAX(totes) AS avg_height
 FROM stacks
 LEFT JOIN scout_data ON scout_data.scout_data_id=stacks.scout_data_id
 GROUP BY team) AS t1
